709967aa0ec98418f38decf8e131d6b502c334b8
Ab1912 <122117358+Ab1912@users.noreply.github.com>
2024-01-04 18:39:25 +0530

    fg

---
+++ Python-tutorial1/casting.py
@@ -0,0 +1,18 @@
+#casting is converting one datatype to another datatype
+
+a="10" #data inside "" will be considered as string
+b="20"
+c=a+b
+print(c)
+
+#data casting
+
+a=int("10")
+b=int("20")
+c=a+b
+print(c)
+
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
---
+++ Python-tutorial1/variable.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+c=a+b
+d="Abi"
+print(c)
+print(type(a))
+print(type(d))
